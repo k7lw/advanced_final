b5f50317c6596fc885e2829634d50064ae28b0f1
Karl Straub <karlstraub7@gmail.com>
2021-06-22 14:43:38 -0400

    addded include libs and added home route

--- driver.js
+++ driver.js
@@ -1,14 +1,15 @@
 
 const express = require('express')
+const path = require('path');
 const app = express()
-const port = 3000
+const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, '/home.html'));
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, '/about.html'));
   })
 
 app.listen(port, () => {
