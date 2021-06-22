15ce9e30c039b679d59ca0dc90cf56b41f78a4b6
Karl Straub <karlstraub7@gmail.com>
2021-06-22 19:08:49 -0400

    Added base layout and scrollspy

--- driver.js
+++ driver.js
@@ -2,15 +2,12 @@
 const express = require('express')
 const path = require('path');
 const app = express()
+app.use(express.static(__dirname+'/public'));
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/home.html'));
+  res.sendFile(path.join(__dirname, '/index.html'));
 })
-
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, '/about.html'));
-  })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
